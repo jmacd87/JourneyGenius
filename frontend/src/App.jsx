b8b819accbf9ed705dffbb596abd425335c35e9a
Joel MacDonald <jmacd87@gmail.com>
2025-02-10 10:09:47 -0800

    add vercel analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import { FlightsProvider } from './context/FlightsContext';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Analytics />
         </Routes>
       </Router>
     </FlightsProvider>
